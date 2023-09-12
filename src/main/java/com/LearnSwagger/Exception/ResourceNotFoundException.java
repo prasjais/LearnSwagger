c8e5078ddd88f4f4804bbca3060ebfb2be0f1c0c
Prashant Jaiswal <prashantj385@gmail.com>
2023-09-12 17:27:32 +0530

    Exception message has been changed.

--- src/main/java/com/LearnSwagger/Exception/ResourceNotFoundException.java
+++ src/main/java/com/LearnSwagger/Exception/ResourceNotFoundException.java
@@ -5,7 +5,7 @@ public class ResourceNotFoundException extends RuntimeException{
     private int id;
     public ResourceNotFoundException(int id)
     {
-        super(String.format("Resource with this id : %i not found",id));
+        super(String.format("User with this id : %i not present in the record",id));
         this.id = id;
     }
 }
